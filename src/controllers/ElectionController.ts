4655e47929d18e07456b3c3b2b70c69c75d34843
Juan Carlos <juanksoliveira@gmail.com>
2020-04-21 16:55:33 -0300

    feat(controller): add unimplemented controllers

---
+++ src/controllers/ElectionController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+export default {
+  async index(req: Request, res: Response) {
+    throw new Error("Method not implemented.");
+  },
+  async create(req: Request, res: Response) {
+    throw new Error("Method not implemented.");
+  },
+  async update(req: Request, res: Response) {
+    throw new Error("Method not implemented.");
+  },
+  async delete(req: Request, res: Response) {
+    throw new Error("Method not implemented.");
+  },
+};
