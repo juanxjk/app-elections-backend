bd4f562361b3215acbec91e54d754bed2a11d491
Juan Carlos <juanksoliveira@gmail.com>
2020-04-26 00:25:20 -0300

    chore(npm): add body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 import routes from "./routes";
 import config from "./config";
 
@@ -8,6 +9,7 @@ import "reflect-metadata";
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use(routes);
 
 if (process.env.NODE_ENV !== "prod")
