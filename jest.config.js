18c9176b3b6cceb161a6f28704cd3cd8412d3a55
Juan Carlos <juanksoliveira@gmail.com>
2020-05-11 01:36:22 -0300

    chore(jest): add jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ["<rootDir>"],
+  testMatch: [
+    "**/__tests__/(unit|integration)/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
