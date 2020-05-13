6d41ac8f371dbdd699f95853508dc293bac5dead
Juan Carlos <juanksoliveira@gmail.com>
2020-05-13 02:03:16 -0300

    feat(user): add bcrypt for password hash

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 export enum UserRole {
   Admin = "admin",
   Surveyor = "surveyor",
@@ -14,9 +15,16 @@ export default class User {
   public id?: number;
   public fullName!: string;
   public username!: string;
-  public password!: string;
+  public set password(password: string) {
+    this.passwordHash = bcrypt.hashSync(password, 10);
+  }
+  private passwordHash!: string;
   public email!: string;
   public role!: UserRole;
   public status!: UserStatus;
   public avatarURL?: string;
+
+  public checkPassword(password: string): boolean {
+    return bcrypt.compareSync(password, this.passwordHash);
+  }
 }
