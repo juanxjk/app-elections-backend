e0e676745cce7345403e70ab93cbe2ea10cb232c
Juan Carlos <juanksoliveira@gmail.com>
2020-05-13 02:15:43 -0300

    feat(db): add User's typeorm entity

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,28 +1,59 @@
 import bcrypt from "bcrypt";
+import { Entity, Column, PrimaryGeneratedColumn, ObjectID } from "typeorm";
+
 export enum UserRole {
   Admin = "admin",
   Surveyor = "surveyor",
-  Guest = "guest"
+  Guest = "guest",
 }
 
 export enum UserStatus {
   Online = "online",
   Surveying = "surveying",
-  Offline = "offline"
+  Offline = "offline",
 }
 
+@Entity({ name: "users" })
 export default class User {
-  public id?: number;
+  @PrimaryGeneratedColumn("uuid")
+  public id?: string;
+
+  @Column()
   public fullName!: string;
+
+  @Column({ nullable: false, unique: true })
   public username!: string;
+
   public set password(password: string) {
     this.passwordHash = bcrypt.hashSync(password, 10);
   }
+
+  @Column({ nullable: false, select: false })
   private passwordHash!: string;
+
+  @Column({ nullable: false, unique: true })
   public email!: string;
+
+  @Column({ nullable: false, default: false })
   public isVerified!: boolean;
+
+  @Column({
+    type: "enum",
+    enum: UserRole,
+    nullable: false,
+    default: UserRole.Guest,
+  })
   public role!: UserRole;
+
+  @Column({
+    type: "enum",
+    enum: UserStatus,
+    nullable: false,
+    default: UserStatus.Offline,
+  })
   public status!: UserStatus;
+
+  @Column({ nullable: true })
   public avatarURL?: string;
 
   public checkPassword(password: string): boolean {
