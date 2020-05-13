8d6c6cf6bf7abfa6d69ba3732c88483eafdc036e
Juan Carlos <juanksoliveira@gmail.com>
2020-05-13 02:12:37 -0300

    feat(user): add isVerified field

--- src/models/User.ts
+++ src/models/User.ts
@@ -20,6 +20,7 @@ export default class User {
   }
   private passwordHash!: string;
   public email!: string;
+  public isVerified!: boolean;
   public role!: UserRole;
   public status!: UserStatus;
   public avatarURL?: string;
