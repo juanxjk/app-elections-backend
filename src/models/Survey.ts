0510b0e79ae6b84f4df04d6eca743cf7b85a4587
Juan Carlos <juanksoliveira@gmail.com>
2020-05-18 01:23:55 -0300

    feat(models): add typeorm entity for Survey

--- src/models/Survey.ts
+++ src/models/Survey.ts
@@ -1,21 +1,59 @@
 import User from "./User";
 import Candidate from "./Candidate";
 import ElectionPoll from "./ElectionPoll";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from "typeorm";
 
 export enum ApprovalType {
   Good = "good",
   Normal = "normal",
-  Bad = "bad"
+  Bad = "bad",
 }
 
+@Entity({ name: "surveys" })
 export default class Survey {
-  public id?: number;
+  @PrimaryGeneratedColumn("uuid")
+  public id!: string;
+
+  @Column({ type: "enum", enum: ApprovalType, nullable: false })
   public approvalType!: ApprovalType;
+
+  @ManyToOne((type) => Candidate, { nullable: false })
   public favoritedCandidate!: Candidate;
+
+  @ManyToOne((type) => Candidate, { nullable: false })
   public notFavoritedCandidate!: Candidate;
+
+  @Column({ nullable: false })
   public surveyDate!: Date;
+
+  @Column({ type: "double precision", nullable: false })
   public latitude?: number;
+
+  @Column({ type: "double precision", nullable: false })
   public longitude?: number;
+
+  @ManyToOne((type) => ElectionPoll, (electionPoll) => electionPoll.surveys, {
+    nullable: false,
+  })
   public electionPoll!: ElectionPoll;
+
+  @ManyToOne((type) => User, { nullable: false })
   public createdBy!: User;
+
+  @CreateDateColumn()
+  public createdAt!: Date;
+
+  @UpdateDateColumn()
+  public updatedAt?: Date;
+
+  @DeleteDateColumn()
+  public deletedAt?: Date;
 }
