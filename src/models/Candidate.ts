6d62cc1f18e2aebf7a923475db68c888ab031c5f
Juan Carlos <juanksoliveira@gmail.com>
2020-05-18 01:23:35 -0300

    feat(models): add typeorm entity for Candidate

--- src/models/Candidate.ts
+++ src/models/Candidate.ts
@@ -1,10 +1,40 @@
 import Election from "./Election";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from "typeorm";
 
+@Entity({ name: "candidates" })
 export default class Candidate {
-  public id?: number;
+  @PrimaryGeneratedColumn("uuid")
+  public id!: string;
+
+  @Column({ nullable: false })
   public name!: string;
+
+  @ManyToOne((type) => Election, (election) => election.candidates)
   public election!: Election;
+
+  @Column()
   public party?: string;
+
+  @Column({ nullable: false })
   public voteNumber!: number;
+
+  @Column()
   public avatarURL?: string;
+
+  @CreateDateColumn()
+  public createdAt!: Date;
+
+  @UpdateDateColumn()
+  public updatedAt?: Date;
+
+  @DeleteDateColumn()
+  public deletedAt?: Date;
 }
